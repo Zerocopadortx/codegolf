de1772fa6f25f6daee358ba8200706870daef053
Zerocopadortx <abregujordialex@gmail.com>
2025-07-27 15:11:22 -0300

    Update arabicToRoman.py

--- arabicToRoman.py
+++ arabicToRoman.py
@@ -1,35 +1,3 @@
-"""""
-romanString = ""
-
-def arabicToRoman(x):
-    global romanString
-    arabicNumbers = [1,5,10,50,100,500,1000]
-    romanNumbers = {
-        1: "I",
-        4: "IV",
-        5: "V",
-        9: "IX",
-        10: "X",
-        40: "XL",
-        50: "L",
-        90: "XC",
-        100: "C",
-        400: "CD",
-        500: "D",
-        900: "CM",
-        1000: "M",
-    }
-    for i in range(1, x + 1):
-        if i in romanNumbers:
-           romanString += romanNumbers[i]       
-
-numero_seleccionado = int(input("select a number from 1 to 4000: "))
-arabicToRoman(numero_seleccionado)
-
-print(romanString)
-
-"""
-
 romanString = ""
 
 def arabicToRoman(x):
